6e1c1e861b47bfb8f278ba17323aab9437791b6d
Praneeth Maldakal <vengalreddysushmitha@gmail.com>
2022-08-18 14:50:22 +0530

    [SUSHMITHA]ADDED:added the contact details of address book system

---
+++ src/com/bridzelab/AddressBookSystem/AddressBook.java
@@ -0,0 +1,7 @@
+package com.bridzelab.AddressBookSystem;
+
+	//implementation of address book system
+public class AddressBook 
+{
+	
+}
---
+++ src/com/bridzelab/AddressBookSystem/AddressBookMain.java
@@ -0,0 +1,13 @@
+package com.bridzelab.AddressBookSystem;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) 
+	{
+		//welcome message for address book
+		System.out.println("===================================");
+		System.out.println("Welcome to Address Book System!!!!!");
+ 
+	}
+
+}
